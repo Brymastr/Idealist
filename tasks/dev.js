37f69fbdb29aff9add7d9c318035c22f5437ec50
da <dannyhaitran@outlook.com>
2015-09-23 20:34:25 -0700

    nodemon and gulp.  error still lives on.

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-  console.log('Gulp is working')
+var runCommand = function(command) {
+  exec(command, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    if (err !== null) {
+      console.log('exec error: ' + err);
+    }
+  });
+}
+//gulp.task('mongo-stop', function() {
+//  runCommand('mongo admin --eval "db.shutdownServer();"');
+//});
+//gulp.task('mongo-start', function() {
+//  runCommand("mongod");
+//});
+gulp.task('run-server', function() {
+  nodemon({
+    script: 'server.js'
+    , ext: 'js html'
+    , evn: {'NODE_ENV' : 'development'}
+  })
 });
+
+gulp.task('default', ['run-server']);
