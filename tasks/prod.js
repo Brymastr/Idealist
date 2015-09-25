b0f647d36cd9b8079c9fea283a4b24175fdafb52
Brycen <dorsay@live.ca>
2015-09-24 19:07:38 -0700

    App minified and concatenated

--- tasks/prod.js
+++ tasks/prod.js
@@ -1,2 +1,16 @@
 var gulp = require('gulp');
-var useref = require('gulp-useref');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+
+gulp.task('build-app-prod', function() {
+  return gulp.src('./app/**/*.js')
+    .pipe(plugins.concat('app.min.js'))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('build-prod', ['clean', 'build-app-prod']);
