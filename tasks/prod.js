097fa8edc659cd95a6af57f216113d9786d50da7
Brycen <dorsay@live.ca>
2015-09-24 19:26:52 -0700

    Config gulp task prod

--- tasks/prod.js
+++ tasks/prod.js
@@ -2,15 +2,21 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
 
-gulp.task('build-app-prod', function() {
+gulp.task('build', ['clean'], function() {
   return gulp.src('./app/**/*.js')
     .pipe(plugins.concat('app.min.js'))
     .pipe(plugins.uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('config', ['clean'], function() {
+  return gulp.src('./config/config.prod.js')
+    .pipe(plugins.uglify({mangle: false}))
+    .pipe(gulp.dest('dist/config/'))
 });
 
 gulp.task('clean', function() {
   return del(['dist']);
 });
 
-gulp.task('build-prod', ['clean', 'build-app-prod']);
+gulp.task('build-prod', ['clean', 'build', 'config']);
