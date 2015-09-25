51de8dc0af078c97772f657e409d6faee3615202
Brycen <dorsay@live.ca>
2015-09-24 20:02:22 -0700

    Gulp task for server and dependencies in prod

--- tasks/prod.js
+++ tasks/prod.js
@@ -12,11 +12,25 @@ gulp.task('build', ['clean'], function() {
 gulp.task('config', ['clean'], function() {
   return gulp.src('./config/config.prod.js')
     .pipe(plugins.uglify({mangle: false}))
-    .pipe(gulp.dest('dist/config/'))
+    .pipe(gulp.dest('dist/config/'));
+});
+
+gulp.task('server', ['clean'], function() {
+  return gulp.src('server.js')
+    .pipe(plugins.uglify({mangle: false}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('dependencies', ['clean'], function() {
+  return gulp.src([
+    './node_modules/**',
+    '!./node_modules/gulp-*'
+  ])
+    .pipe(gulp.dest('dist/libs/'));
 });
 
 gulp.task('clean', function() {
   return del(['dist']);
 });
 
-gulp.task('build-prod', ['clean', 'build', 'config']);
+gulp.task('build-prod', ['clean', 'build', 'server', 'config', 'dependencies']);
